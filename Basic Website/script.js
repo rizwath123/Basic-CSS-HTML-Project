2db20eee10406f7e6e50eebe6ae6c6d60ca3f0cb
rizwath123 <rizwathr@gmail.com>
2023-01-02 22:25:06 +0300

    stopwatch created

---
+++ Basic Website/script.js
@@ -0,0 +1,38 @@
+let [seconds, minutes, hours] = [0, 0, 0]
+let displayTime = document.getElementById("displayTime")
+let timer = null
+function stopwatch() {
+    seconds++;
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes == 60) {
+            minutes = 0;
+            hours++;
+            if (hours == 24) {
+                restart()
+            }
+        }
+    }
+    let h = hours < 10 ? '0' + hours : hours;
+    let m = minutes < 10 ? '0' + minutes : minutes;
+    let s = seconds < 10 ? '0' + seconds: seconds;
+    displayTime.innerHTML = h+ ":" + m+ ":" + s;
+}
+function watchStart(){
+    if(timer !== null){
+        clearInterval(timer);
+        }
+    timer = setInterval(stopwatch,1000);
+}
+function stop(){
+    clearInterval(timer); 
+    alert("The Timer is Stopped")
+}
+function restart(){
+    clearInterval(timer);
+    seconds = 0
+    minutes = 0
+    hours = 0 
+    displayTime.innerHTML = "00:00:00"
+}
